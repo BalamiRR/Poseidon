f0f83a709bb4bcfb8057ba03ac55a88209713c04
BalamiRR <fuatkara.95@gmail.com>
2025-09-11 17:00:23 +0200

    home

--- src/main/java/com/nnk/springboot/controllers/HomeController.java
+++ src/main/java/com/nnk/springboot/controllers/HomeController.java
@@ -1,14 +1,19 @@
 package com.nnk.springboot.controllers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/home")
 @Controller
 public class HomeController
 {
-	@RequestMapping("/")
-	public String home(Model model) {
+	@GetMapping
+	public String home(Authentication authentication, Model model) {
+		String name = authentication.getName();
+		model.addAttribute("name", "Logged in as: " + name);
 		return "home";
 	}
 
