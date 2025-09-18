5c7243f855e0c58f5ac0cd556a988f877d733371
BalamiRR <fuatkara.95@gmail.com>
2025-09-18 14:37:19 +0200

    All implementation for BidList UI

---
+++ src/main/java/com/nnk/springboot/services/BidListServiceImpl.java
@@ -0,0 +1,21 @@
+package com.nnk.springboot.services;
+
+import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.repositories.BidListRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class BidListServiceImpl implements BidListService {
+    private final BidListRepository bidListRepository;
+
+    @Override
+    public List<BidList> findAll() {
+        return bidListRepository.findAll();
+    }
+}
