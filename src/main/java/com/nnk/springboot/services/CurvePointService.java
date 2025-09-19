40d7f44ec91a329394cceae42de5b15e7c3a9788
BalamiRR <fuatkara.95@gmail.com>
2025-09-19 21:47:10 +0200

    curvePoint business implementation

---
+++ src/main/java/com/nnk/springboot/services/CurvePointService.java
@@ -0,0 +1,10 @@
+package com.nnk.springboot.services;
+
+import com.nnk.springboot.domain.CurvePoint;
+
+import java.util.List;
+
+public interface CurvePointService {
+    List<CurvePoint> findAll();
+
+}
---
+++ src/main/java/com/nnk/springboot/services/CurvePointServiceImpl.java
@@ -0,0 +1,21 @@
+package com.nnk.springboot.services;
+
+import com.nnk.springboot.domain.CurvePoint;
+import com.nnk.springboot.repositories.CurvePointRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class CurvePointServiceImpl implements CurvePointService {
+    private final CurvePointRepository curvePointRepository;
+
+    @Override
+    public List<CurvePoint> findAll() {
+        return curvePointRepository.findAll();
+    }
+}
