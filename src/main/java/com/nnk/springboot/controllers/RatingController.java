9a3d1f9bf27c5e506fabf40c4e4e408077ffb74f
BalamiRR <fuatkara.95@gmail.com>
2025-09-23 21:20:14 +0200

    Rating Controller update

--- src/main/java/com/nnk/springboot/controllers/RatingController.java
+++ src/main/java/com/nnk/springboot/controllers/RatingController.java
@@ -1,24 +1,27 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.Rating;
+import com.nnk.springboot.services.RatingService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@RequiredArgsConstructor
 @Controller
 public class RatingController {
+    private final RatingService ratingService;
     // TODO: Inject Rating service
 
-    @RequestMapping("/rating/list")
+    @GetMapping("/rating/list")
     public String home(Model model)
     {
         // TODO: find all Rating, add to model
+        model.addAttribute("ratings", ratingService.findAll());
         return "rating/list";
     }
 
