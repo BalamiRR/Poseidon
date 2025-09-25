14f73a26da0c21ed72c79084b34920337dccd09f
BalamiRR <fuatkara.95@gmail.com>
2025-09-25 19:59:55 +0200

    serviceImpl FindAll

---
+++ src/main/java/com/nnk/springboot/services/TradeServiceImpl.java
@@ -0,0 +1,22 @@
+package com.nnk.springboot.services;
+
+import com.nnk.springboot.domain.Trade;
+import com.nnk.springboot.repositories.TradeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class TradeServiceImpl implements TradeService {
+
+    private final TradeRepository tradeRepository;
+
+    @Override
+    public List<Trade> findAll() {
+        return tradeRepository.findAll();
+    }
+}
