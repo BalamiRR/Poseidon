1020d3ecc054faf6be5511bfd15519d4d066e384
BalamiRR <fuatkara.95@gmail.com>
2025-09-26 21:47:39 +0200

    Service implementation

---
+++ src/main/java/com/nnk/springboot/services/RuleNameServiceImpl.java
@@ -0,0 +1,21 @@
+package com.nnk.springboot.services;
+
+import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.repositories.RuleNameRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class RuleNameServiceImpl implements RuleNameService{
+    private final RuleNameRepository ruleNameRepository;
+
+    @Override
+    public List<RuleName> findAll() {
+        return ruleNameRepository.findAll();
+    }
+}
