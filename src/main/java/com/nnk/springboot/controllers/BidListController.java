ad2d65ee7155779271b6e76a641005e4dbc497e4
BalamiRR <fuatkara.95@gmail.com>
2025-09-30 16:20:00 +0200

    unnecessary space delete

--- src/main/java/com/nnk/springboot/controllers/BidListController.java
+++ src/main/java/com/nnk/springboot/controllers/BidListController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @Controller
 public class BidListController {
-    // TODO: Inject Bid service
     private final BidListService bidListService;
 
     @GetMapping("/bidList/list")
