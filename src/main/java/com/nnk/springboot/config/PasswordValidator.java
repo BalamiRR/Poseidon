158e2b2ac72f10cf92672c282ac0155feb731e81
BalamiRR <fuatkara.95@gmail.com>
2025-09-30 16:17:29 +0200

    update

--- src/main/java/com/nnk/springboot/config/PasswordValidator.java
+++ src/main/java/com/nnk/springboot/config/PasswordValidator.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class PasswordValidator {
-    private static final String PASSWORD_PATTERN = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#$@!%&*?])[A-Za-z\\d#$@!%&*?]{8,}$";
+    private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=*])(?=\\S+$).{8,}$";
 
     private static final Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
 
