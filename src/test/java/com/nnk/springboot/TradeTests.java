d40057669ce294992ffaa70c752c500adcc12f2d
BalamiRR <fuatkara.95@gmail.com>
2025-10-08 12:07:41 +0200

    Testing the Pojo of Trade

--- src/test/java/com/nnk/springboot/TradeTests.java
+++ src/test/java/com/nnk/springboot/TradeTests.java
@@ -2,45 +2,51 @@ package com.nnk.springboot;
 
 import com.nnk.springboot.domain.Trade;
 import com.nnk.springboot.repositories.TradeRepository;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
+import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.Optional;
 
-@RunWith(SpringRunner.class)
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 public class TradeTests {
 
 	@Autowired
 	private TradeRepository tradeRepository;
 
+	@BeforeEach
+	public void setup() {
+		tradeRepository.deleteAll();
+	}
+
 	@Test
 	public void tradeTest() {
-		Trade trade = new Trade("Trade Account", "Type");
+		Trade tradee = new Trade("TradeAccount", "Type");
+		tradee.setTradeId(1);
+
+		Trade trade = new Trade("TradeAccount", "Type", 10.0);
 
 		// Save
 		trade = tradeRepository.save(trade);
-		Assert.assertNotNull(trade.getTradeId());
-		Assert.assertTrue(trade.getAccount().equals("Trade Account"));
+		assertNotNull(trade.getTradeId());
+		assertEquals("TradeAccount", trade.getAccount());
 
 		// Update
-		trade.setAccount("Trade Account Update");
+		trade.setAccount("TradeAccountUpdate");
 		trade = tradeRepository.save(trade);
-		Assert.assertTrue(trade.getAccount().equals("Trade Account Update"));
+		assertEquals("TradeAccountUpdate", trade.getAccount());
 
 		// Find
 		List<Trade> listResult = tradeRepository.findAll();
-		Assert.assertTrue(listResult.size() > 0);
+		assertTrue(listResult.size() > 0);
 
 		// Delete
 		Integer id = trade.getTradeId();
 		tradeRepository.delete(trade);
 		Optional<Trade> tradeList = tradeRepository.findById(id);
-		Assert.assertFalse(tradeList.isPresent());
+		assertFalse(tradeList.isPresent());
 	}
 }
