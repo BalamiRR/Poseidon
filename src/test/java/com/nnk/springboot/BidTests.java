431188650cdd8584d39992cb262977432228e803
BalamiRR <fuatkara.95@gmail.com>
2025-10-08 12:08:03 +0200

    Testing the Pojo of BidList

--- src/test/java/com/nnk/springboot/BidTests.java
+++ src/test/java/com/nnk/springboot/BidTests.java
@@ -2,17 +2,12 @@ package com.nnk.springboot;
 
 import com.nnk.springboot.domain.BidList;
 import com.nnk.springboot.repositories.BidListRepository;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
+import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.Optional;
-
-@RunWith(SpringRunner.class)
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class BidTests {
@@ -22,26 +17,35 @@ public class BidTests {
 
 	@Test
 	public void bidListTest() {
-		BidList bid = new BidList("Account Test", "Type Test", 10d);
+		BidList bid = new BidList("AccountTest", "TypeTest", 10d);
 
 		// Save
 		bid = bidListRepository.save(bid);
-		Assert.assertNotNull(bid.getBidListId());
-		Assert.assertEquals(bid.getBidQuantity(), 10d, 10d);
+		assertNotNull(bid.getBidListId());
+		assertEquals(10d, bid.getBidQuantity(), 0.01);
+		assertEquals("AccountTest", bid.getAccount());
+		assertEquals("TypeTest", bid.getType());
 
 		// Update
+		bid.setAccount("Account");
+		bid.setType("Type");
 		bid.setBidQuantity(20d);
 		bid = bidListRepository.save(bid);
-		Assert.assertEquals(bid.getBidQuantity(), 20d, 20d);
+		assertEquals(bid.getBidQuantity(), 20d, 20d);
+		assertEquals(bid.getAccount(), "Account", "Account");
+		assertEquals(bid.getType(), "Type", "Type");
 
 		// Find
 		List<BidList> listResult = bidListRepository.findAll();
-		Assert.assertTrue(listResult.size() > 0);
+		assertTrue(listResult.size() > 0);
 
 		// Delete
 		Integer id = bid.getBidListId();
 		bidListRepository.delete(bid);
 		Optional<BidList> bidList = bidListRepository.findById(id);
-		Assert.assertFalse(bidList.isPresent());
+		assertFalse(bidList.isPresent());
+
+		BidList bidd = new BidList("AccountTest", "TypeTest", 10d);
+		bidd.setBidListId(1);
 	}
 }
