66e76ceb6ca35bd7216eef05ab6bae932631dbbb
BalamiRR <fuatkara.95@gmail.com>
2025-10-10 11:21:51 +0200

    adding example of encoding the password in case

--- src/main/java/com/nnk/springboot/Application.java
+++ src/main/java/com/nnk/springboot/Application.java
@@ -2,12 +2,17 @@ package com.nnk.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//		String rawPassword = "Admin@123";
+//		String hashedPassword = encoder.encode(rawPassword);
+//		System.out.println(hashedPassword);
 	}
 
 }
