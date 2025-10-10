a76b921e471b5cf46cabad0b15e31c7b92aa31f9
BalamiRR <fuatkara.95@gmail.com>
2025-10-10 11:23:24 +0200

    service test

---
+++ src/test/java/com/nnk/springboot/service/BidListServiceTest.java
@@ -0,0 +1,62 @@
+package com.nnk.springboot.service;
+
+import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.repositories.BidListRepository;
+import com.nnk.springboot.services.BidListService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BidListServiceTest {
+
+    BidList bidList = new BidList();
+
+    @Autowired
+    private BidListService bidListService;
+
+    @Autowired
+    private BidListRepository bidListRepository;
+
+    @BeforeAll
+    public void initBidList() {
+        bidList.setAccount("account");
+        bidList.setType("type");
+        bidList.setBidQuantity(10.0);
+        bidListService.insertBidList(bidList);
+    }
+
+    @AfterAll
+    public void cleanBidList(){
+        bidListRepository.deleteAll();
+    }
+
+    @Test
+    public void test_findAllBidList(){
+        List<BidList> lists = bidListService.findAll();
+        assertFalse(lists.isEmpty());
+    }
+
+    @Test
+    public void test_getBidListById (){
+        Integer id = bidList.getBidListId();
+        BidList foundId = bidListService.findById(id);
+        assertEquals("account", foundId.getAccount());
+        assertEquals("type", foundId.getType());
+        assertEquals(10.0, foundId.getBidQuantity(), 0.01);
+    }
+
+    @Test
+    public void test_deleteById() {
+        Integer id = bidList.getBidListId();
+        bidListService.deleteById(id);
+        BidList byId = bidListService.findById(id);
+        assertNull(byId);
+    }
+}
